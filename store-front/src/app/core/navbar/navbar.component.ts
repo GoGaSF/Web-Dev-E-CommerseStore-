d31b7e7b84597fdf808cf8533b80f2c26a85e114
PoloTester <zhambulaie@gmail.com>
2025-04-22 21:05:59 +0500

    other components in home

--- store-front/src/app/core/navbar/navbar.component.ts
+++ store-front/src/app/core/navbar/navbar.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { SearchComponent } from '../search/search.component';
+// import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive, SearchComponent],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  toggleSearch() {
+    // Implement search toggle functionality
+    console.log('Search toggled');
+  }
 }
