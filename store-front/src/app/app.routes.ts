232627a487e766b0311c0128581b3bc5d413dcbd
PoloTester <zhambulaie@gmail.com>
2025-04-23 15:30:03 +0500

    pages

--- store-front/src/app/app.routes.ts
+++ store-front/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { MenComponent } from './pages/men/men.component';
+import { WomenComponent } from './pages/women/women.component';
+import { MenAccessoriesComponent } from './pages/men-accessories/men-accessories.component';
+import { MenShoesComponent } from './pages/men-shoes/men-shoes.component';
+import { MenClothComponent } from './pages/men-cloth/men-cloth.component';
+
+
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: '**', redirectTo: '' },
     { path: 'home', component: HomeComponent },
-    
+    { path: 'men', component: MenComponent },
+    {path: 'men/accessories', component: MenAccessoriesComponent},
+    {path: 'men/shoes', component: MenShoesComponent},
+    {path: 'men/cloth', component: MenClothComponent},
+    { path: 'women', component: WomenComponent },
+    { path: '**', redirectTo: '' },
 ];
--- store-front/src/app/pages/men/men.component.ts
+++ store-front/src/app/pages/men/men.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-men',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './men.component.html',
   styleUrl: './men.component.css'
 })
 export class MenComponent {
-
+  
 }
