41649e364cc6634d85f4d375dfc71acdbfb16da4
PoloTester <zhambulaie@gmail.com>
2025-04-23 18:07:33 +0500

    back edit

---
+++ store-backend/products/views.py
@@ -0,0 +1,35 @@
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['category', 'brand']
+    ordering_fields = ['price', 'name']
+    
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        
+        # Filter by category
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category=category)
+        
+        # Filter by brand
+        brands = self.request.query_params.getlist('brand')
+        if brands:
+            queryset = queryset.filter(brand__in=brands)
+        
+        # Filter by price range
+        min_price = self.request.query_params.get('min_price')
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+            
+        max_price = self.request.query_params.get('max_price')
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)
+            
+        return queryset
